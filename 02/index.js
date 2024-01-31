b5b0dc3b044674f9883283b992e02af4e688c967
Adams Lech <adams.a.lech@hotmail.com>
2024-01-30 23:46:26 -0300

    atualizacao ex02

--- 02/index.js
+++ 02/index.js
@@ -1,4 +1,9 @@
-const jogada1 = 5;
+const jogada1 = 9;
 const jogada2 = 3;
 
-//seu código aqui
+const somaJogadas = (jogada1 + jogada2);
+if (somaJogadas % 2 == 0) {
+    console.log('Par');
+} else {
+    console.log('Impar');
+}
