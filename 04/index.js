df1a5e57930f442aa45d460be237ad579e6da356
Adams Lech <adams.a.lech@hotmail.com>
2024-02-06 22:02:07 -0300

    exercicio 4 feito

---
+++ 04/index.js
@@ -0,0 +1,21 @@
+let ladoA = 12;
+let ladoB = 12;
+
+if (ladoA === 0 && ladoB === 0) {
+    console.log('Branco');
+} else if (ladoA === 1 && ladoB === 1) {
+    console.log('Ás');
+} else if (ladoA === 2 && ladoB === 2) {
+    console.log('Duque');
+} else if (ladoA === 3 && ladoB === 3) {
+    console.log('Terno');
+} else if (ladoA === 4 && ladoB === 4) {
+    console.log('Quadra');
+} else if (ladoA === 5 && ladoB === 5) {
+    console.log('Quina');
+} else if (ladoA === 6 && ladoB === 6) {
+    console.log('Sena');
+} else {
+    console.log('Jogada Inexistente');
+}
+console.log('FIM');
